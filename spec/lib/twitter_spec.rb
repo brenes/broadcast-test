baf0aa229b1645bd61c0929d863fe004b2e9ec82
David J. Brenes <davidjbrenes@gmail.com>
2014-12-20 00:04:29 +0100

    Moving all the YAML stuff to testing (Now we really have a thin wrapper around the Twitter gem)

--- spec/lib/twitter_spec.rb
+++ spec/lib/twitter_spec.rb
@@ -1,13 +1,18 @@
+require 'yaml'
+
 describe Broadcast::Twitter do
 
   context "when broadcasting something to Twitter" do
 
+    let(:twitter_config) { YAML.load_file( File.expand_path('../config/twitter.yml', File.dirname(__FILE__)) )['twitter'] }
     let (:client) { Broadcast::Twitter.client }
     let (:previous_status_count) { client.user.tweets_count }
     let(:tweet) { client.user.status }
     let(:tweet_message) { "just testing something #{Time.now}" }
 
     before do
+      Broadcast::Twitter::configure twitter_config
+
       previous_status_count
       Broadcast::Twitter.broadcast tweet_message
     end
