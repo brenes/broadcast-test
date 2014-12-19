5405781d30eef36e33f73f340b7316d9915ae65d
David J. Brenes <davidjbrenes@gmail.com>
2014-12-20 00:57:41 +0100

    And now a spec testing that you can broadcast to both services!

--- spec/lib/share_spec.rb
+++ spec/lib/share_spec.rb
@@ -8,22 +8,37 @@ describe Broadcast::Share do
     let (:twitter_client) { Broadcast::Twitter.client }
     let (:previous_status_count) { twitter_client.user.tweets_count }
     let(:tweet) { twitter_client.user.status }
+
+    let(:tumblr_config) { YAML.load_file( File.expand_path('../config/tumblr.yml', File.dirname(__FILE__)) )['tumblr'] }
+    let(:tumblr_url) { tumblr_config["tumblr_url"] }
+    let (:client) { Broadcast::Tumblr.client }
+    let (:previous_post_count) { client.blog_info(tumblr_url)["blog"]["posts"] }
+    let(:post) { client.posts(tumblr_url, limit: 1)["posts"].first }
+
     let(:message) { "just testing something #{Time.now}" }
 
     before do
       Broadcast::Twitter::configure twitter_config
+      Broadcast::Tumblr::configure tumblr_config
 
       previous_status_count
-      Broadcast::Share.to message, :twitter
+      previous_post_count
+
+      Broadcast::Share.to message, :twitter, :tumblr
     end
 
     after do
       twitter_client.destroy_status tweet
+      client.delete tumblr_url, post["id"]
     end
 
     it "should be broadcasted" do
+
       expect(twitter_client.user.tweets_count).to eq (previous_status_count + 1)
       expect(tweet.text).to eq message
+
+      expect(client.blog_info(tumblr_url)["blog"]["posts"]).to eq (previous_post_count + 1)
+      expect(post["title"]).to eq message
     end
 
   end
